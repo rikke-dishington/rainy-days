d278ef26c6325131af1d940611e814994e093827
rikke-dishington <85433495+rikke-dishington@users.noreply.github.com>
2022-10-01 20:52:21 +0200

    Form validation and accessibility updates

---
+++ js/contact.js
@@ -0,0 +1,45 @@
+const form = document.querySelector("#contactForm");
+const fullName = document.querySelector("#fullName");
+const fullNameError = document.querySelector("#fullNameError");
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailError");
+const address = document.querySelector("#message");
+const addressError = document.querySelector("#message");
+
+function submitForm(event) {
+  event.preventDefault();
+
+  if (checkLength(fullName.value, 0) === true) {
+    fullNameError.style.display = "none";
+  } else {
+    fullNameError.style.display = "block";
+  }
+
+  if (validateEmail(email.value) === true) {
+    emailError.style.display = "none";
+  } else {
+    emailError.style.display = "block";
+  }
+
+  if (checkLength(message.value, 10) === true) {
+    messageError.style.display = "none";
+  } else {
+    messageError.style.display = "block";
+  }
+}
+
+form.addEventListener("submit", submitForm);
+
+function checkLength(value, len) {
+  if (value.trim().length > len) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validateEmail(email) {
+  const regEx = /\S+@\S+\.\S+/;
+  const patternMatches = regEx.test(email);
+  return patternMatches;
+}
